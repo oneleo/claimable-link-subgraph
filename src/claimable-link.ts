eea0fe79a16d84f0d9c034550addeb248fd17138
陳敬翔 <irara.chen@token.im>
2025-04-09 13:48:50 +0800

    style: Format the claimable-link.ts code

--- src/claimable-link.ts
+++ src/claimable-link.ts
@@ -8,8 +8,8 @@ import {
   Paused as PausedEvent,
   Refunded as RefundedEvent,
   SignerUpdated as SignerUpdatedEvent,
-  Unpaused as UnpausedEvent
-} from "../generated/ClaimableLink/ClaimableLink"
+  Unpaused as UnpausedEvent,
+} from "../generated/ClaimableLink/ClaimableLink";
 import {
   Cancelled,
   Claimed,
@@ -20,57 +20,57 @@ import {
   Paused,
   Refunded,
   SignerUpdated,
-  Unpaused
-} from "../generated/schema"
+  Unpaused,
+} from "../generated/schema";
 
 export function handleCancelled(event: CancelledEvent): void {
   let entity = new Cancelled(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.giver = event.params.giver
-  entity.token = event.params.token
-  entity.transferID = event.params.transferID
+  );
+  entity.giver = event.params.giver;
+  entity.token = event.params.token;
+  entity.transferID = event.params.transferID;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleClaimed(event: ClaimedEvent): void {
   let entity = new Claimed(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.giver = event.params.giver
-  entity.token = event.params.token
-  entity.transferID = event.params.transferID
-  entity.amount = event.params.amount
-  entity.recipient = event.params.recipient
-  entity.signer = event.params.signer
+  );
+  entity.giver = event.params.giver;
+  entity.token = event.params.token;
+  entity.transferID = event.params.transferID;
+  entity.amount = event.params.amount;
+  entity.recipient = event.params.recipient;
+  entity.signer = event.params.signer;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleDeposited(event: DepositedEvent): void {
   let entity = new Deposited(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.giver = event.params.giver
-  entity.token = event.params.token
-  entity.transferID = event.params.transferID
-  entity.amount = event.params.amount
-  entity.expiration = event.params.expiration
+  );
+  entity.giver = event.params.giver;
+  entity.token = event.params.token;
+  entity.transferID = event.params.transferID;
+  entity.amount = event.params.amount;
+  entity.expiration = event.params.expiration;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleEIP712DomainChanged(
@@ -78,13 +78,13 @@ export function handleEIP712DomainChanged(
 ): void {
   let entity = new EIP712DomainChanged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOwnershipTransferStarted(
@@ -92,15 +92,15 @@ export function handleOwnershipTransferStarted(
 ): void {
   let entity = new OwnershipTransferStarted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOwnershipTransferred(
@@ -108,68 +108,68 @@ export function handleOwnershipTransferred(
 ): void {
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handlePaused(event: PausedEvent): void {
   let entity = new Paused(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
+  );
+  entity.account = event.params.account;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRefunded(event: RefundedEvent): void {
   let entity = new Refunded(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.giver = event.params.giver
-  entity.token = event.params.token
-  entity.transferID = event.params.transferID
+  );
+  entity.giver = event.params.giver;
+  entity.token = event.params.token;
+  entity.transferID = event.params.transferID;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleSignerUpdated(event: SignerUpdatedEvent): void {
   let entity = new SignerUpdated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.signer = event.params.signer
-  entity.isActivated = event.params.isActivated
+  );
+  entity.signer = event.params.signer;
+  entity.isActivated = event.params.isActivated;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleUnpaused(event: UnpausedEvent): void {
   let entity = new Unpaused(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
+  );
+  entity.account = event.params.account;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
